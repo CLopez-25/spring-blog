5322bc3c5540848353598c7284db6af9a930e9a7
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-15 17:10:43 -0600

    almost done with controllers exercises

---
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -0,0 +1,34 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @GetMapping("/add/{x}/and/{y}")
+    @ResponseBody
+    public double addNumbers(@PathVariable double x, @PathVariable double y ){
+        return x + y;
+    }
+
+    @GetMapping("/subtract/{x}/from/{y}")
+    @ResponseBody
+    public double subtract(@PathVariable double x, @PathVariable double y ){
+        return x - y;
+    }
+
+    @GetMapping("/multiply/{x}/and/{y}")
+    @ResponseBody
+    public double multiply(@PathVariable double x, @PathVariable double y){
+        return x * y;
+    }
+
+    @GetMapping("/divide/{x}/and/{y}")
+    @ResponseBody
+    public double divide(@PathVariable double x, @PathVariable double y){
+        return x / y;
+    }
+
+}
