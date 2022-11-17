d1dc45905a1b812599bb592423bbb395b2b0b167
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-17 09:16:07 -0600

    modyfied dicecontroller

--- src/main/java/com/codeup/springblog/controllers/DiceController.java
+++ src/main/java/com/codeup/springblog/controllers/DiceController.java
@@ -9,13 +9,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DiceController {
     @GetMapping("/roll-dice")
-    @ResponseBody
-    public String guess(){
-        return "guess";
+    public String diceRoll(){
+        return "roll-dice";
     }
 
     @GetMapping("/roll-dice/{n}")
-    @ResponseBody
     public String diceGuess(@PathVariable int n, Model model){
         int randomNumber = (int) (Math.random() * 6) + 1;
         boolean check = randomNumber == n;
