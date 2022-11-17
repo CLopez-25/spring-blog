6f683990a569168724ca81f61cc0da2a4dcc2819
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-16 22:34:51 -0600

    created dice controller

--- src/main/java/com/codeup/springblog/controllers/DiceController.java
+++ src/main/java/com/codeup/springblog/controllers/DiceController.java
@@ -1,7 +1,9 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,5 +12,16 @@ public class DiceController {
     @ResponseBody
     public String guess(){
         return "guess";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    @ResponseBody
+    public String diceGuess(@PathVariable int n, Model model){
+        int randomNumber = (int) (Math.random() * 6) + 1;
+        boolean check = randomNumber == n;
+        model.addAttribute("n", n);
+        model.addAttribute("randomNumber", randomNumber);
+        model.addAttribute("check", check);
+        return "dice-roll-check";
     }
 }
