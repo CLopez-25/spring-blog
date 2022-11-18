57260a112dd4c4c2f9ead73bf71352d204c216c3
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-17 23:03:04 -0600

    troubleshooting dice

--- src/main/java/com/codeup/springblog/controllers/DiceController.java
+++ src/main/java/com/codeup/springblog/controllers/DiceController.java
@@ -1,25 +1,24 @@
 package com.codeup.springblog.controllers;
 
+//import com.codeup.springblog.models.Coffee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/roll-dice")
 public class DiceController {
-    @GetMapping("/roll-dice")
+    @GetMapping
     public String diceRoll(){
         return "roll-dice";
     }
 
-    @GetMapping("/roll-dice/{n}")
-    public String diceGuess(@PathVariable int n, Model model){
-        int randomNumber = (int) (Math.random() * 6) + 1;
-        boolean check = randomNumber == n;
-        model.addAttribute("n", n);
-        model.addAttribute("randomNumber", randomNumber);
-        model.addAttribute("check", check);
+    @GetMapping("/{number}")
+    public String diceGuess(@PathVariable String number, Model model){
+        model.addAttribute("number", number);
+        model.addAttribute("randomNumber",Math.ceil(Math.random()*6));
         return "roll-dice";
     }
 }
