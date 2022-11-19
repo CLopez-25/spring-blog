c2373f2f8aa2eef4a5e148d7080fdcb3bc7024e3
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-19 15:47:17 -0600

    added id getter setter

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,25 +1,40 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+@RequestMapping("/posts")
 public class PostController {
-    @GetMapping("/posts")
-    @ResponseBody
-    public String allPosts(){
-        return "Here are all the posts:...";
+    @GetMapping
+    public String allPosts(Model model){
+        Post post1 = new Post(1, "First", "This is my first post!!");
+        Post post2 = new Post(2, "Second", "This is the second post");
+        Post post3 = new Post(3, "Yo", "Third post?");
+        List<Post> allPosts = new ArrayList<>(List.of(post1, post2, post3));
+        Post post = null;
+        for (Post userPost : allPosts){
+            if (userPost.getId() == id){
+                post = userPost;
+            }
+        }
+        return "/post/";
     }
 
-    @GetMapping("/posts/{id}")
-    @ResponseBody
+    @GetMapping("/{id}")
     public String onePost(@PathVariable long id){
         return "Here is post number: " + id;
     }
 
-    @GetMapping("/posts/create")
+    @GetMapping("/create")
     @ResponseBody
     public String createPost(){
         return "Here is the form to create a post!";
