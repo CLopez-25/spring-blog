3fbebe50dc952f5cf5da5f49d94118f7da90ca83
Cesar Lopez <lopez.m.cesar17@gmail.com>
2022-11-23 16:26:34 -0600

    finishing repo and jpa exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -4,10 +4,7 @@ import com.codeup.springblog.models.Post;
 import com.codeup.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,32 +21,27 @@ public class PostController {
 
     @GetMapping
     public String allPosts(Model model){
-        Post post1 = new Post(1, "First", "This is my first post!!");
-        Post post2 = new Post(2, "Second", "This is the second post");
-        List<Post> allPosts = new ArrayList<>(List.of(post1, post2));
+        List<Post> allPosts = postDao.findAll();
         model.addAttribute("allPosts", allPosts);
         return "/posts/index";
     }
 
     @GetMapping("/{id}")
     public String onePost(@PathVariable long id, Model model){
-        Post post1 = new Post(1, "First", "This is my first post!!");
-        Post post2 = new Post(2, "Second", "This is the second post");
-        Post post3 = new Post(3, "Yo", "Third post?");
-        List<Post> allPosts = new ArrayList<>(List.of(post1, post2, post3));
-        Post post = null;
-        for (Post userPost : allPosts){
-            if (userPost.getId() == id){
-                post = userPost;
-            }
-        }
+        Post post = postDao.findById(id);
         model.addAttribute("post", post);
         return "/posts/show";
     }
 
     @GetMapping("/create")
-    @ResponseBody
     public String createPost(){
-        return "Here is the form to create a post!";
+        return "/posts/create";
+    }
+
+    @PostMapping("/create")
+    public String submitPost(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
+        Post post = new Post(title, body);
+        postDao.save(post);
+        return "redirect:/posts";
     }
 }
